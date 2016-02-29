324e817a7bb2dcf0e84a536ae28dd66e1bdaf5e0
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-02-29 16:53:08 +0300

    Add AemPageManager

---
+++ SapAutomation.AemPageManager/AemPage.cs
@@ -0,0 +1,18 @@
+﻿namespace SapAutomation.AemPageManager
+{
+    using QA.AutomatedMagic;
+    using QA.AutomatedMagic.MetaMagic;
+
+    [MetaType("AemPage")]
+    public class AemPage : BaseMetaObject
+    {
+        [MetaTypeValue("Aem page title")]
+        public string Title { get; set; } = null;
+
+        [MetaTypeValue("Parent aem page path")]
+        public string ParentPath { get; set; } = null;
+
+        [MetaTypeValue("Aem page template")]
+        public string Template { get; set; } = null;
+    }
+}
---
+++ SapAutomation.AemPageManager/AemPageManager.cs
@@ -0,0 +1,54 @@
+﻿namespace SapAutomation.AemPageManager
+{
+    using QA.AutomatedMagic;
+    using QA.AutomatedMagic.CommandsMagic;
+    using System;
+
+    [CommandManager(Description = "Manager for aem pages")]
+    public class AemPageManager : BaseCommandManager
+    {
+        public AemPageManager()
+            : base(null)
+        { }
+
+        [Command("Generate command for aem page creation", "CreatePageCmd")]
+        public string GenerateCommandForCreatePage(AemPage aemPage, ILogger log)
+        {
+            try
+            {
+                log?.DEBUG($"Generate command for aem page '{aemPage.Title}' creation");
+
+                var tmp = $"?cmd=createPage&parentPath={aemPage.ParentPath}&title={aemPage.Title}&template={aemPage.Template}";
+
+                log?.DEBUG($"Generating command for aem page '{aemPage.Title}' creation completed");
+
+                return tmp;
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred during generating command for aem page '{aemPage.Title}' creation");
+                throw new CommandAbortException($"Error occurred during generating command for aem page '{aemPage.Title}' creation", ex);
+            }
+        }
+
+        [Command("Generate command for aem page activation", "ActivatePageCmd")]
+        public string GenerateCommandForActivatePage(AemPage aemPage, ILogger log)
+        {
+            try
+            {
+                log?.DEBUG($"Generate command for aem page '{aemPage.Title}' activation");
+
+                var tmp = $"?cmd=Activate&path={aemPage.ParentPath}/{aemPage.Title}";
+
+                log?.DEBUG($"Generating command for aem page '{aemPage.Title}' activation completed");
+
+                return tmp;
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred during generating command for aem page '{aemPage.Title}' activation");
+                throw new CommandAbortException($"Error occurred during generating command for aem page '{aemPage.Title}' activation", ex);
+            }
+        }
+    }
+}
