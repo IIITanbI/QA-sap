cc6edcba49807d418e42e40f2c61c325b9454b52
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-02-29 17:38:08 +0300

    Sync

---
+++ SapAutomation.Pages/Components/Facets/FacetsElementData.cs
@@ -0,0 +1,15 @@
+﻿namespace SapAutomation.Pages.Components.Facets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using QA.AutomatedMagic;
+    using QA.AutomatedMagic.MetaMagic;
+
+    [MetaType("Config for facets data")]
+    public class FacetsElementData : BaseMetaObject
+    {
+    }
+}
