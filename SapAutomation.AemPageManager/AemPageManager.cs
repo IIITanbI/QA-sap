82be805d80a1fac1a416c171cb6de5047d589a73
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-03-01 18:18:01 +0300

    Update AemPageManager

--- SapAutomation.AemPageManager/AemPageManager.cs
+++ SapAutomation.AemPageManager/AemPageManager.cs
@@ -2,27 +2,31 @@
 {
     using QA.AutomatedMagic;
     using QA.AutomatedMagic.CommandsMagic;
+    using QA.AutomatedMagic.ApiManager;
     using System;
 
-    [CommandManager(Description = "Manager for aem pages")]
-    public class AemPageManager : BaseCommandManager
+    [CommandManager("Manager for aem pages")]
+    public class AemPageManager : ICommandManager
     {
-        public AemPageManager()
-            : base(null)
-        { }
-
-        [Command("Generate command for aem page creation", "CreatePageCmd")]
-        public string GenerateCommandForCreatePage(AemPage aemPage, ILogger log)
+        [Command("Create page", "CreatePage")]
+        public void CreatePage(ApiManager apiManager, AemPage aemPage, ILogger log)
         {
             try
             {
-                log?.DEBUG($"Generate command for aem page '{aemPage.Title}' creation");
+                log?.DEBUG($"Create page with title:' {aemPage.Title}'");
 
-                var tmp = $"?cmd=createPage&parentPath={aemPage.ParentPath}&title={aemPage.Title}&template={aemPage.Template}";
+                var cmd = $"?cmd=createPage&parentPath={aemPage.ParentPath}&title={aemPage.Title}&template={aemPage.Template}";
 
-                log?.DEBUG($"Generating command for aem page '{aemPage.Title}' creation completed");
+                log?.TRACE($"Command for page creation: {cmd}");
 
-                return tmp;
+                var request = new Request
+                {
+                    ContentType = "text/html;charset=UTF-8",
+                    Method = Request.Methods.POST,
+                    PostData = cmd
+                };
+
+                apiManager.PerformRequest(request, log);
             }
             catch (Exception ex)
             {
