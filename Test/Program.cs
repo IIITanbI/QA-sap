c434951cdbc039ce14dc94dbfa05933f388644bd
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-03-01 15:00:46 +0300

    Initial components for tutorial catalog

---
+++ Test/Program.cs
@@ -0,0 +1,55 @@
+﻿using QA.AutomatedMagic;
+using QA.AutomatedMagic.ApiManager;
+using SapAutomation.AemPageManager;
+using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void DEBUG(string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ERROR(string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void FATAL(string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void INFO(string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void LOG(string level, string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void TRACE(string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WARN(string message, Exception exception = null)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
