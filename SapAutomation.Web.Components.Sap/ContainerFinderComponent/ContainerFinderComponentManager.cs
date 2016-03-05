c790c6a8f88cb6c428ec904482f458e581e318b1
Kiryl_Sakalouski <kiryl_sakalouski@epam.com>
2016-03-05 17:10:46 +0300

    REFACTORALLLLLITY!!!!!! final edition

---
+++ SapAutomation.Web.Components.Sap/ContainerFinderComponent/ContainerFinderComponentManager.cs
@@ -0,0 +1,70 @@
+﻿namespace SapAutomation.Web.Components.Sap.ContainerFinderComponent
+{
+    using Aem.InsertNewComponentForm;
+    using FacetsComponent;
+    using FinderResultsComponent;
+    using QA.AutomatedMagic;
+    using QA.AutomatedMagic.CommandsMagic;
+    using QA.AutomatedMagic.WebDriverManager;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [CommandManager("Container finder manager")]
+    public class ContainerFinderComponentManager : BaseCommandManager
+    {
+        public FinderResultsComponentManager FinderResultsManager { get; set; }
+        public FacetsComponentManager FacetsManager { get; set; }
+
+        [MetaSource(nameof(ContainerFinderComponent) + @"\ContainerFinderComponentWebDefinition.xml")]
+        public WebElement ContainerFinderComponentWebDefinition { get; set; }
+
+        public ContainerFinderComponentManager()
+        {
+            FinderResultsManager = AutomatedMagicManager.CreateCommandManager<FinderResultsComponentManager>();
+            FacetsManager = AutomatedMagicManager.CreateCommandManager<FacetsComponentManager>();
+        }
+
+        public override void Init()
+        {
+            ContainerFinderComponentWebDefinition.ChildWebElements.Add(FinderResultsManager.FinderResultsComponentWebDefinition);
+            ContainerFinderComponentWebDefinition.ChildWebElements.Add(FacetsManager.FacetsComponentWebDefinition);
+        }
+
+        [Command("Command for setup container finder", "SetUpContainerFinder")]
+        public void SetUpContainerFinder(WebDriverManager webDriverManager, ContainerFinderComponentConfig containerFinderComponentConfig, ILogger log)
+        {
+            webDriverManager.Click(ContainerFinderComponentWebDefinition["ContainerFinder.EditContainerFinder"], log);
+            webDriverManager.Click(ContainerFinderComponentWebDefinition["ContainerFinder.ContainerFinderEdit.PathConfiguration"], log);
+
+            foreach (var path in containerFinderComponentConfig.PagePaths)
+            {
+                webDriverManager.Click(ContainerFinderComponentWebDefinition["ContainerFinder.ContainerFinderEdit.PathAdd"], log);
+                webDriverManager.SendKeys(ContainerFinderComponentWebDefinition["ContainerFinder.ContainerFinderEdit.PathField"], path, log);
+                webDriverManager.Click(ContainerFinderComponentWebDefinition["ContainerFinder.ContainerFinderEdit.PathConfiguration"], log);
+            }
+            webDriverManager.Click(ContainerFinderComponentWebDefinition["ContainerFinder.ContainerFinderEdit.EditorOK"], log);
+
+            webDriverManager.Refresh(log);
+            webDriverManager.WaitForPageLoaded(log);
+            webDriverManager.WaitForJQueryLoaded(log);
+        }
+
+        [Command("Command for add facets component", "AddFacetsComponent")]
+        public void AddFacetsComponent(WebDriverManager webDriverManager, InsertNewComponentFormManager insertNewComponentFormManager, ILogger log)
+        {
+            webDriverManager.ActionsDoubleClick(ContainerFinderComponentWebDefinition[$"ContainerFinderPage.DragFacets"], log);
+            insertNewComponentFormManager.AddComponent(webDriverManager, "FacetsComponent", log);
+        }
+
+        [Command("Command for add finder results component", "AddFinderResultsComponent")]
+        public void AddFinderResultsComponent(WebDriverManager webDriverManager, InsertNewComponentFormManager insertNewComponentFormManager, ILogger log)
+        {
+            webDriverManager.ActionsDoubleClick(ContainerFinderComponentWebDefinition[$"ContainerFinderPage.DragFinderResult"], log);
+            insertNewComponentFormManager.AddComponent(webDriverManager, "FinderResultComponent", log);
+        }
+    }
+}
---
+++ SapAutomation.Web.Components.Sap/FacetsComponent/FacetsComponentManager.cs
@@ -0,0 +1,47 @@
+﻿namespace SapAutomation.Web.Components.Sap.FacetsComponent
+{
+    using QA.AutomatedMagic;
+    using QA.AutomatedMagic.CommandsMagic;
+    using QA.AutomatedMagic.WebDriverManager;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [CommandManager("Facets manager")]
+    public class FacetsComponentManager : BaseCommandManager
+    {
+        [MetaSource(nameof(FacetsComponent) + @"\FacetsComponentWebDefinition.xml")]
+        public WebElement FacetsComponentWebDefinition { get; set; }
+
+        [Command("Command for setup facets", "SetUpFacets")]
+        public void SetUpFacets(WebDriverManager webDriverManager, FacetsComponentConfig facetsComponentConfig, ILogger log)
+        {
+            webDriverManager.Click(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditButton"], log);
+
+            foreach (var nameSpace in facetsComponentConfig.Namespaces)
+            {
+                webDriverManager.Click(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.AddNamespaces"], log);
+                webDriverManager.SendKeys(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.LastPath"], nameSpace, log);
+                webDriverManager.Click(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.FacetsTab"], log);
+                webDriverManager.SendKeys(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.LastDefaultValue"], "1", log);
+            }
+
+            if (facetsComponentConfig.HideFacets)
+            {
+                webDriverManager.CheckCheckbox(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.HideFacets"], log);
+            }
+            else
+            {
+                webDriverManager.UnCheckCheckbox(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.HideFacets"], log);
+            }
+
+            webDriverManager.Click(FacetsComponentWebDefinition[$"FacetsElement.FacetsElementEditor.TypeOfSelection.{facetsComponentConfig.TypeOfSelection.ToString()}"], log);
+            webDriverManager.Click(FacetsComponentWebDefinition["FacetsElement.FacetsElementEditor.EditorOK"], log);
+
+            webDriverManager.WaitForPageLoaded(log);
+            webDriverManager.WaitForJQueryLoaded(log);
+        }
+    }
+}
