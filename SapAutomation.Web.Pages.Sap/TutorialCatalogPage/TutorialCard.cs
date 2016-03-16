119329ba22e7242e1cb89d94c72caf98b2d8e803
Aliaksandr_Rasolka <aliaksandr_rasolka@epam.com>
2016-03-16 17:34:00 +0300

    Extend TutorialCatalogPageManager

---
+++ SapAutomation.Web.Pages.Sap/TutorialCatalogPage/TutorialCard.cs
@@ -0,0 +1,28 @@
+﻿namespace SapAutomation.Web.Pages.Sap.TutorialCatalogPage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using QA.AutomatedMagic.MetaMagic;
+
+    [MetaType("Tutorial card")]
+    public class TutorialCard : BaseMetaObject
+    {
+        [MetaTypeValue("Tutorial card title", IsRequired = false)]
+        public string Title { get; set; } = null;
+
+        [MetaTypeValue("Tutorial card description", IsRequired = false)]
+        public string Description{ get; set; } = null;
+
+        [MetaTypeValue("Tutorial card url", IsRequired = false)]
+        public string URL { get; set; } = null;
+
+        [MetaTypeCollection("Tutorial card tags", IsRequired = false)]
+        public List<string> Tags { get; set; } = null;
+
+        [MetaTypeValue("Tutorial card status", IsRequired = false)]
+        public string Status { get; set; } = null;
+    }
+}
--- SapAutomation.Web.Pages.Sap/TutorialCatalogPage/TutorialCatalogPageManager.cs
+++ SapAutomation.Web.Pages.Sap/TutorialCatalogPage/TutorialCatalogPageManager.cs
@@ -29,5 +29,78 @@
             TutorialCatalogPageWebDefenition.ChildWebElements.Add(TutorialCatalogManager.TutorialCatalogComponentWebDefinition);
             TutorialCatalogPageWebDefenition.Init();
         }
+
+        [Command("Get tutorial card list on author")]
+        public List<TutorialCard> GetTutorialCardsOnAuthor(WebDriverManager webDriverManager, ILogger log)
+        {
+            try
+            {
+                log?.INFO($"Get tutorial card list'");
+                var list = new List<TutorialCard>();
+
+                var cards = webDriverManager.FindElements(TutorialCatalogPageWebDefenition["Root.TutorialCard"], log);
+                log?.DEBUG($"Find '{cards.Count}' cards'");
+
+                var tutorialCard = new TutorialCard();
+                foreach (var card in cards)
+                {
+                    try
+                    {
+                        log?.TRACE("Try to get card title");
+                        tutorialCard.Title = webDriverManager.FindElement(card, TutorialCatalogPageWebDefenition["Root.TutorialCard.Title"], log).Text;
+                        log?.TRACE($"Card title is: {tutorialCard.Title}");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Error occurred during parsing card Title. Card index: {cards.IndexOf(card)}", ex);
+                    }
+                    try
+                    {
+                        log?.TRACE("Try to get card URL");
+                        tutorialCard.URL = webDriverManager.FindElement(card, TutorialCatalogPageWebDefenition["Root.TutorialCard.Title"], log).GetAttribute("href");
+                        log?.TRACE($"Card URL is: {tutorialCard.URL}");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Error occurred during parsing card URL. Cart title: {tutorialCard.Title}", ex);
+                    }
+                    try
+                    {
+                        log?.TRACE("Try to get card tags");
+                        var tags = webDriverManager.FindElements(TutorialCatalogPageWebDefenition["Root.TutorialCard.Tag"], log);
+                        List<string> tg = new List<string>();
+                        foreach (var tag in tags)
+                        {
+                            tg.Add(tag.Text);
+                        }
+                        tutorialCard.Tags = tg;
+                        log?.TRACE($"Card tags is: {tutorialCard.Tags.ToString()}");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Error occurred during parsing card tags. Cart title: {tutorialCard.Title}", ex);
+                    }
+                    try
+                    {
+                        log?.TRACE("Try to get card status");
+                        tutorialCard.Status = webDriverManager.FindElement(card, TutorialCatalogPageWebDefenition["Root.TutorialCard.Status"], log).Text;
+                        log?.TRACE($"Card status is: {tutorialCard.Status}");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Error occurred during parsing card status. Cart title: {tutorialCard.Title}", ex);
+                    }
+                    list.Add(tutorialCard);
+                }
+
+                log?.INFO($"Getting tutorial card list on author completed");
+                return list;
+            }
+            catch (Exception ex)
+            {
+                log?.ERROR($"Error occurred during getting tutorial cards on author");
+                throw new CommandAbortException($"Error occurred during getting tutorial cards on author", ex);
+            }
+        }
     }
 }
